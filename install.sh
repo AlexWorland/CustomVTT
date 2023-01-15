c8c638a8a0c994d1b7d9d6efb42f43371fb4d811
Alex Worland <alexworland@gmail.com>
2023-01-14 21:54:29 -0800

    add execution permissions to install

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ apt update
 yes | apt install git jq curl net-tools openssl
 curl -fsSL https://deb.nodesource.com/setup_19.x | bash - &&\
 apt-get install -y nodejs
-cd /app/Application/resources/app
+cd Application/resources/app
 npm install
