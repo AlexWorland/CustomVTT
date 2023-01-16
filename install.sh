eef351366f59bbfce6b9ddb3b0283ee3dea8331c
AlexWorland <alexworland@gmail.com>
2023-01-15 22:08:20 -0800

    add sudo to install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt update
-yes | apt install git jq curl net-tools openssl
-curl -fsSL https://deb.nodesource.com/setup_19.x | bash - &&\
-apt-get install -y nodejs
+sudo apt update
+yes | sudo apt install git jq curl net-tools openssl
+curl -fsSL https://deb.nodesource.com/setup_19.x | sudo bash - 
+sudo apt-get install -y nodejs
 cd Application/resources/app
 npm install
